61ad0cdc40264aaabeccfee77a27ed9d3caaac86
Carolina Borges Cardoso <borges_carolina@sicredi.com.br>
2022-04-25 11:28:50 -0300

    criando teste para o cenário 1: salario > 1000

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,24 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+
+    @Test //Cenário 1: salario > 10000 => bônus = 0
+    public void bonusDeveSerZeroParaSalarioAcimaDeDezMil() {
+        BonusService bonusService = new BonusService();
+        Funcionario funcionario = new Funcionario("Carol", LocalDate.now(), new BigDecimal(12000));
+
+        BigDecimal bonus = bonusService.calcularBonus(funcionario);
+
+        assertEquals(BigDecimal.ZERO, bonus);
+
+    }
+
+}
